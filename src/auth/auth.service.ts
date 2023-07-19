91b4c9d834280f63e7a10f0067e03febc6d9cf05
Abdullah Ilker UGUDUCU <abdullahilkeruguducu@gmail.com>
2023-07-19 12:43:15 +0300

    register endpointi ayarlandı mongodb bağlanmıyor

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AuthDto } from './dto/auth.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class AuthService {
-  register() {
-    return 'register page';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async register(dto: AuthDto) {
+    const newUser = new this.userModel({
+      email: dto.email,
+      password: dto.password,
+    });
+
+    return await newUser.save();
   }
 }
